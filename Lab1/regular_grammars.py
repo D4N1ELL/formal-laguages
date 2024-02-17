c66db55bc21340dbbd3920a638f67415a9b633ab
Istrati Daniel <danielistrati767@gmail.com>
2024-02-17 20:49:02 +0200

    initial setup

---
+++ Lab1/regular_grammars.py
@@ -0,0 +1,13 @@
+# Istrati Daniel FAF-221
+# Variant 16:
+# VN={S, A, B},
+# VT={a, b, c, d},
+# P={
+#     S → bS
+#     S → dA
+#     A → aA
+#     A → dB
+#     B → cB
+#     A → b
+#     B → a
+# }
